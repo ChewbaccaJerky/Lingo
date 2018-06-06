e2096a0215ffbf4f8d7db56ec34e3decfff3d6f5
John Robin <johnrobinlugtu@gmail.com>
2018-06-05 23:18:05 -0700

    Basic file structure finished

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./client/src/index.js",
+    output: {
+        path: path.resolve(__dirname, "client", "public", "javascript"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    }
+};
